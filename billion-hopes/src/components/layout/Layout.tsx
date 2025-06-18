d7299efad37bc39dc74fd2b9e82964bb8f6e46f9
Ricky <kapoortoursandtravelsindore@gmail.com>
2025-06-18 17:03:32 +0530

    Everything is working - saving this version

--- billion-hopes/src/components/layout/Layout.tsx
+++ billion-hopes/src/components/layout/Layout.tsx
@@ -2,17 +2,37 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../../components/Navbar';
 import Sidebar from './Sidebar';
+import SocialMedia from '../SocialMedia';
 
 const Layout: React.FC = () => {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="flex flex-col min-h-screen bg-gray-50">
       <Navbar />
-      <div className="flex pt-24">
+      <div className="flex flex-1 pt-24">
         <Sidebar />
-        <main className="flex-1 ml-64 p-8">
-          <Outlet />
-        </main>
+        <div className="flex-1 ml-64">
+          <main className="p-8 pb-24">
+            <Outlet />
+          </main>
+          
+          {/* Footer with Social Media */}
+          <footer className="bg-blue-600 border-t w-full text-white">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+              <div className="flex flex-col md:flex-row justify-between items-center gap-4">
+                <div className="text-white/90 whitespace-nowrap">
+                  © {new Date().getFullYear()} Billion Hopes. All rights reserved.
+                </div>
+                <div className="text-white">
+                  <SocialMedia variant="footer" />
+                </div>
+              </div>
+            </div>
+          </footer>
+        </div>
       </div>
+      
+      {/* Sticky Social Media Panel */}
+      <SocialMedia variant="sticky" />
     </div>
   );
 };
